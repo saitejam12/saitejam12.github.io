c066738b9c04bd269ca7ca2fea7273c667c304ec
saitejam12 <saitejamuddana@gmail.com>
2019-11-16 22:41:57 -0600

    windows update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import DemoLinks from './DemoLinks';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h6>How To Demo.Me</h6>
       </header>
+      <DemoLinks />
     </div>
   );
 }
---
+++ src/DemoLinks.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DemoLinks = () => {
+  return <div>Howtoplay</div>;
+};
+
+export default DemoLinks;
