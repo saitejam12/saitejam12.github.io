296fcf5fe6fa7679f72675b538fdc3fcd86f7f5b
saitejam12 <saitejamuddana@gmail.com>
2019-11-16 22:45:59 -0600

    Updates

---
+++ precache-manifest.0635364619847e02fd20b2faa915be1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f10305089f8809951c9661ceb8131c61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e50323164a664010131",
+    "url": "/static/css/main.381295a7.chunk.css"
+  },
+  {
+    "revision": "6d299a76d9cded235b6a",
+    "url": "/static/js/2.317d17d0.chunk.js"
+  },
+  {
+    "revision": "6e50323164a664010131",
+    "url": "/static/js/main.80f5bd93.chunk.js"
+  },
+  {
+    "revision": "72a84f117d6750b4276a",
+    "url": "/static/js/runtime-main.7b5fe7ef.js"
+  },
+  {
+    "revision": "6c19b6662f11b1e38ca2c46bb62b578c",
+    "url": "/static/media/tsunami.6c19b666.jpg"
+  }
+]);
