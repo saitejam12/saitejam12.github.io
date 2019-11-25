946437c969f0e8d81fcce6d09a62903f279d3c93
saitejam12 <saitejamuddana@gmail.com>
2019-11-24 23:48:32 -0600

    replace old with new site

---
+++ src/containers/App/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>How To Demo.me</p>
+      </header>
+      <div className='App-Body-1'>
+        <p>Paragraph 1</p>
+      </div>
+      <div className='App-Body-2'>
+        <p>Paragraph 2</p>
+      </div>
+      <div className='App-Body-3'>
+        <p>Paragraph 3</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
